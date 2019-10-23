982fc4ca3008fa0def28f334bf4a950a1ac6a186
Jorge <jdmonge9@institutosanmartin.edu.ar>
2019-10-23 20:17:47 -0300

    probando1

--- app/src/main/java/com/example/tpfinalmonge/MainActivity.java
+++ app/src/main/java/com/example/tpfinalmonge/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.tpfinalmonge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button Prueba;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Prueba = findViewById(R.id.Prueba);
+        Prueba.setOnClickListener( this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v.getId() == Prueba.getId())
+        {
+            Toast.makeText(getApplicationContext(),"TIRO LIBRE PARA BOCA",Toast.LENGTH_LONG).show();
+        }
+
     }
 }
